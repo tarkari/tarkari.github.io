0a63b4373218c4adb58bedd5284937c4652aa5ca
Himlay <contact@himalay.com.np>
2020-09-19 13:12:45 +0545

    Convert es6 syntax to es5 for better browser support

--- assets/app.js
+++ assets/app.js
@@ -4,17 +4,21 @@ if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
     navigator.serviceWorker
       .register("/serviceWorker.js")
-      .then(() => console.log("service worker registered"))
-      .catch((err) => console.log("service worker not registered", err));
+      .then(function () {
+        return console.log("service worker registered");
+      })
+      .catch(function (err) {
+        return console.log("service worker not registered", err);
+      });
   });
 }
 
 (function () {
   var lang = localStorage.getItem("lang") || "np";
-  var currency = { np: "रू", en: "Rs" };
   var currentTheme = localStorage.getItem("theme") || "dark";
   var themeSwitch = document.querySelector('.theme input[type="checkbox"]');
   var langSwitch = document.querySelector('.lang input[type="checkbox"]');
+  var currency = { np: "रू", en: "Rs" };
   var min = { en: "Min", np: "न्यूनतम" };
   var max = { en: "Max", np: "अधिकतम" };
 
@@ -66,7 +70,7 @@ if ("serviceWorker" in navigator) {
         return res.text();
       })
       .then(function (html) {
-        const rootEl = document.querySelector("#root");
+        var rootEl = document.querySelector("#root");
         var tempDom = Object.assign(document.createElement("div"), {
           innerHTML: html,
         });
@@ -76,38 +80,74 @@ if ("serviceWorker" in navigator) {
           headerData[1].textContent.trim()
         );
         rootEl.innerHTML = "";
-        for (var row of tempDom.querySelectorAll('[class*="row"]')) {
-          var data = {
-            name: row.children[0].textContent.trim(),
-            unit: row.children[1].textContent.trim(),
-            min: row.children[2].textContent.trim(),
-            max: row.children[3].textContent.trim(),
-            avg: row.children[4].textContent.trim(),
-          };
-          rootEl.innerHTML += /*html*/ `<div class="card">
-      <h3>${data.name}</h3> 
-      <div>
-          <div>
-              <span class="price">${currency[lang]} ${data.avg}</span>
-              <small class="unit">/${data.unit}</small>
-          </div>
-          <div class="min-max">
-              <span title="${min[lang]}"><span class="min" ></span> <span class="text">${currency[lang]} ${data.min}</span></span>
-              <span title="${max[lang]}"><span class="max"></span> <span class="text">${currency[lang]} ${data.max}</span></span>
-          </div>
-      </div>
-    </div>`;
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
+
+        try {
+          for (
+            var _iterator = tempDom
+                .querySelectorAll('[class*="row"]')
+                [Symbol.iterator](),
+              _step;
+            !(_iteratorNormalCompletion = (_step = _iterator.next()).done);
+            _iteratorNormalCompletion = true
+          ) {
+            var row = _step.value;
+
+            var data = {
+              name: row.children[0].textContent.trim(),
+              unit: row.children[1].textContent.trim(),
+              min: row.children[2].textContent.trim(),
+              max: row.children[3].textContent.trim(),
+              avg: row.children[4].textContent.trim(),
+            };
+            rootEl.innerHTML +=
+              '<div class="card"><h3>' +
+              data.name +
+              '</h3><div><div><span class="price">' +
+              currency[lang] +
+              " " +
+              data.avg +
+              '</span><small class="unit">/' +
+              data.unit +
+              '</small></div><div class="min-max"><span title="' +
+              min[lang] +
+              '"><span class="min" ></span> <span class="text">' +
+              currency[lang] +
+              " " +
+              data.min +
+              '</span></span><span title="' +
+              max[lang] +
+              '"><span class="max"></span> <span class="text">' +
+              currency[lang] +
+              " " +
+              data.max +
+              "</span></span></div></div></div>";
+          }
+        } catch (err) {
+          _didIteratorError = true;
+          _iteratorError = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+              _iterator.return();
+            }
+          } finally {
+            if (_didIteratorError) {
+              throw _iteratorError;
+            }
+          }
         }
       });
   }
 
   function setTitles(subtitle, date) {
-    const title = lang === "np" ? "तरकारी" : "Tarkari";
+    var title = lang === "np" ? "तरकारी" : "Tarkari";
     date = date.replace("-", ", ");
-    document.title = `${title} - ${subtitle} (${date})`;
+    document.title = title + " - " + subtitle + " (" + date + ")";
     document.querySelector(".title").innerHTML = title;
-    document.querySelector(
-      ".subtitle"
-    ).innerHTML = `${subtitle} <span class="date">(${date})</span>`;
+    document.querySelector(".subtitle").innerHTML =
+      subtitle + ' <span class="date">(' + date + ")</span>";
   }
 })();
